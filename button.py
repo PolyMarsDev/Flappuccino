7224dd1bab165b987d47724680c5b09ee0d32762
amirmistik <57581726+amirmistikad@users.noreply.github.com>
2022-12-23 00:44:19 +0500

    Refactor code

--- button.py
+++ button.py
@@ -1,7 +1,11 @@
 import pygame
+
 class Button:
-    def __init__(self):
-        self.price = 3
-        self.level = 1   
-    sprite = pygame.image.load('data/gfx/button.png')
-    typeIndicatorSprite = pygame.image.load('data/gfx/null_indicator.png')
+    def __init__(self, price = 3, level = 1, sprite = None, indicator = None):
+        self.price = price
+        self.level = level
+        self.sprite = pygame.image.load(sprite)
+        self.indicator = pygame.image.load(indicator)
+        
+    def change_indicator(self, path = None):
+        self.indicator = pygame.image.load(path)
