68f91819c201411d32e98a5bcc5851c27b9334cf
Jacob Knox <jacobknoxa@gmail.com>
2023-01-21 20:58:07 -0500

    Moving stuff from main() and adding spaces

--- main.py
+++ main.py
@@ -15,15 +15,39 @@ from button import Button
 from bean import Bean
 from utils import *
 
+# initialize the game
 pygame.init()
+
 # set the display
 Display.set_caption('Flappuccino')
 Display.set_icon(Bean.sprite)
-DISPLAY = Display.set_mode((640,480),pygame.RESIZABLE | pygame.SCALED,32)
+DISPLAY = Display.set_mode((640, 480), pygame.RESIZABLE | pygame.SCALED,32)
+
 player = Player()
-# get sounds
-flapfx = Sound("data/sfx/flap.wav")
+
+# get fonts
+font = Font('data/fonts/font.otf', 100)
+font_small = Font('data/fonts/font.otf', 32)
+font_20 = Font('data/fonts/font.otf', 20)
+
+# get some images
+shop = Image('data/gfx/shop.png')
+shop_bg = Image('data/gfx/shop_bg.png')
+retry_button = Image('data/gfx/retry_button.png')
+logo = Image('data/gfx/logo.png')
+title_bg = Image('data/gfx/bg.png')
+title_bg.fill((255, 30.599999999999998, 0.0), special_flags=pygame.BLEND_ADD)
+shadow = Image('data/gfx/shadow.png')
 indicators = ['data/gfx/flap_indicator.png', 'data/gfx/speed_indicator.png', 'data/gfx/beanup_indicator.png']
+
+# get some sounds
+upgradefx = Sound("data/sfx/upgrade.wav")
+beanfx = Sound("data/sfx/bean.wav")
+deadfx = Sound("data/sfx/dead.wav")
+flapfx = Sound("data/sfx/flap.wav")
+
+# colors
+WHITE=(255,255,255) # constant
 
 def start():
     global bean_multiplier, beans, buttons, last_time, clicked, jump, dt, mouse_x, mouse_y, scroll
@@ -43,7 +67,7 @@ def start():
     buttons[2].set_price(30)
     # getting 5 beans
     for i in range(5):
-        beans.append(Bean(random.randrange(0, DISPLAY.get_width() - Bean().sprite.get_width()) ,i*-200 - player.position.y))
+        beans.append(Bean(random.randrange(0, DISPLAY.get_width() - Bean().sprite.get_width()) ,i * -200 - player.position.y))
     Sound.play(flapfx)
 
 def func_one(toggle: bool = True) -> None:
@@ -65,7 +89,7 @@ def func_one(toggle: bool = True) -> None:
 def event_handler() -> None:
     global jump, clicked
     for event in pygame.event.get():
-        if event.type==pygame.KEYDOWN and event.key==K_SPACE:
+        if event.type==pygame.KEYDOWN and event.key == K_SPACE:
             jump = True
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             clicked = True
@@ -78,29 +102,14 @@ def event_handler() -> None:
 def main() -> None:
     global clicked, bean_multiplier, beans, scroll
     start()
-    # get fonts
-    font = Font('data/fonts/font.otf', 100)
-    font_small = Font('data/fonts/font.otf', 32)
-    font_20 = Font('data/fonts/font.otf', 20)
-    # get some images
-    shop = Image('data/gfx/shop.png')
-    shop_bg = Image('data/gfx/shop_bg.png')
-    retry_button = Image('data/gfx/retry_button.png')
-    logo = Image('data/gfx/logo.png')
-    title_bg = Image('data/gfx/bg.png')
-    title_bg.fill((255, 30.599999999999998, 0.0), special_flags=pygame.BLEND_ADD)
-    shadow = Image('data/gfx/shadow.png')
-    upgradefx = Sound("data/sfx/upgrade.wav")
-    beanfx = Sound("data/sfx/bean.wav")
-    deadfx = Sound("data/sfx/dead.wav")
-    # colors
-    WHITE=(255,255,255) # constant
+    
     # creating a list of backgrounds, with each index being an object
     bg = [Background(), Background(), Background()]
     # startingHeight = 100 (the initial y position of the player)
     starting_height = player.position.y
+    
+    # splash screen
     splash_screen_timer = 0
-    #splash screen
     while splash_screen_timer < 100:
         func_one(False)
         splash_screen_timer += dt
@@ -113,8 +122,8 @@ def main() -> None:
         # wait for 10 seconds
         pygame.time.delay(10)
     
-    title_screen = True
     # title screen
+    title_screen = True
     Sound.play(flapfx)
     while title_screen:
         func_one()
@@ -154,7 +163,6 @@ def main() -> None:
         current_height_marker = font.render(str(player.height), True, (color[0]*255, color[1]*255, color[2]*255, 50 ))
         DISPLAY.blit(current_height_marker, (DISPLAY.get_width()/2 - current_height_marker.get_width()/2, cam_offset + round((player.position.y - starting_height)/DISPLAY.get_height())*DISPLAY.get_height() + player.current_sprite.get_height() - 40))
         
-        
         for bean in beans:
             DISPLAY.blit(bean.sprite, (bean.position.x, bean.position.y + cam_offset))
         
@@ -170,8 +178,10 @@ def main() -> None:
             level_display = font_20.render(f'Lvl. {button.level}', True, (200,200,200))
             DISPLAY.blit(level_display, (234 + (button.index*125), 441))
             DISPLAY.blit(button.type_indicator_sprite, (202 + (button.index*125), 377))
+            
         bean_count_display = font_small.render(str(player.bean_count).zfill(7), True, (0,0,0))
         DISPLAY.blit(bean_count_display, (72, 394))
+        
         if player.dead:
             DISPLAY.blit(retry_button, (4, 4))
             death_message = font_small.render("RETRY", True, (0, 0, 0))
